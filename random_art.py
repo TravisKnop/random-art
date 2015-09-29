e3d6ce96b9f79294577995321f0e1accc51646ee
TravisKnop <travisknop@gmail.com>
2015-09-29 10:54:46 -0400

    Art created from randomized numbers

--- random_art.py
+++ random_art.py
@@ -9,7 +9,7 @@ import math
 def create_expression():
     """This function takes no arguments and returns an expression that
     generates a number between -1.0 and 1.0, given x and y coordinates."""
-    return random.choice([new_func3()])
+    return random.choice([new_func4(), new_func5(), new_func6()])
 
 
 def run_expression(expr, x, y):
@@ -25,9 +25,18 @@ def new_func():
 
 def new_func2():
     a = random.random()
-    return lambda x, y: a*math.sin(x/a)) * math.tan((x-y**(random.randint(1,10))/a)/a + random.randint(-2,2)
+    return lambda x, y: a*math.sin(x/a) * math.tan((x-y**(random.randint(1,10))/a)/a + random.randint(-2,2))
 
 def new_func3():
     a = random.randint(1,6)
     b = random.randint(1,8)
     return lambda x, y: math.tan(a*(x)**2+a*y**2) * math.sin(b*x**a+y**b) + random.randint(-2,2)
+
+def new_func4():
+    return lambda x, y: math.tan(3*x+1)
+
+def new_func5():
+    return lambda x, y: math.tan(3*x+2)
+
+def new_func6():
+    return lambda x, y: math.tan(3*x)
